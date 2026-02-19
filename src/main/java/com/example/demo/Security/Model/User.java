47aa3137539b78bd06bafaf2310e48da378d3501
Divya Patel <divyagabani19@gmail.com>
2026-02-19 16:09:54 +0530

    Added Database for Loginuser

---
+++ src/main/java/com/example/demo/Security/Model/User.java
@@ -0,0 +1,59 @@
+package com.example.demo.Security.Model;
+
+
+
+
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.demo.Security.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "LoginUser")
+
+public class User implements UserDetails{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String userId;
+	private String name;
+	private String email;
+	
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+	@Override
+	public @Nullable String getPassword() {
+
+		return null;
+	}
+	@Override
+	public String getUsername() {
+
+		return null;
+	}
+}
+
