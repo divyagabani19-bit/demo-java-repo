326548b260d0a0a46299d1556c727cc8560d33b0
Divya Patel <divyagabani19@gmail.com>
2026-02-20 12:24:38 +0530

    changed in jpa query

--- src/main/java/com/example/demo/repository/ProductRepo.java
+++ src/main/java/com/example/demo/repository/ProductRepo.java
@@ -14,9 +14,9 @@ import com.example.demo.model.Product;
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
-	@Query("SELECT p from Product p WHERE " + "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword,'%')) OR " +
-//"LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword,'%')) OR " +
-			"LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword,'%') ) OR "
+	@Query("SELECT p from Product p WHERE " + "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword,'%')) OR "
+			+ "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword,'%')) OR "
+			+ "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword,'%') ) OR "
 			+ "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword,'%'))")
 
 	List<Product> searchProducts(@Param("keyword") String keyword);
